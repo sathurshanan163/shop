07d7a0499a3c2fd4ffe336879146b95c506d92ce
Sathurshanan Kandhasamy <sath7samy@gmail.com>
2025-06-21 16:14:30 +1000

    Register route and controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -19,4 +19,27 @@ const login = async_handler(async (req, res) => {
     };
 });
 
-export {login};
+const register = async_handler(async (req, res) => {
+    const {name, email, password} = req.body;
+    const is_user_exist = await User.findOne({email});
+    if (is_user_exist) {
+        res.status(400);
+        throw new Error("User already exists");
+    };
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+    if (user) {
+        res.status(201).json({
+           ...user,
+           token: generate_token(user._id)
+        });        
+    } else {
+        res.status(400);
+        throw new Error("Invalid user data");
+    };
+});
+
+export {login, register};
